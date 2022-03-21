aae55b424aa2789dd0cae29970d30127946117da
hyle746 <93382394+hyle746@users.noreply.github.com>
2022-03-22 10:43:21 +1300

    test
    test

---
+++ WIS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WIS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
